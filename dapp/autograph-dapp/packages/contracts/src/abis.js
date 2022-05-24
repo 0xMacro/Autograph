d1d45b890519f71f40db6b5b5b90c356387ac52b
dd0sxx <contacttheotelonis@gmail.com>
2022-05-24 15:41:10 -0600

    had to restore default abis to have the app load

--- dapp/autograph-dapp/packages/contracts/src/abis.js
+++ dapp/autograph-dapp/packages/contracts/src/abis.js
@@ -1,9 +1,13 @@
+import erc20Abi from "./abis/erc20.json";
+import ownableAbi from "./abis/ownable.json";
 import AddressBookAbi from "./abis/AddressBook.json";
 import AddressBookDepolyerAbi from "./abis/AddressBookDeployer.json";
 import AddressBookFactoryAbi from "./abis/AddressBookFactory.json";
 import SubscriptionsAbi from "./abis/Subscriptions.json";
 
 const abis = {
+  erc20: erc20Abi,
+  ownable: ownableAbi,
   AddressBook: AddressBookAbi,
   AddressBookDepolyer: AddressBookDepolyerAbi,
   AddressBookFactory: AddressBookFactoryAbi,
@@ -11,3 +15,12 @@ const abis = {
 };
 
 export default abis;
+
+
+/*
+
+const abis = {
+};
+
+export default abis;
+*/
