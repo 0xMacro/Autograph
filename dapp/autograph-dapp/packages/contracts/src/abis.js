af3dc50bebdc977601bb9965c961aa23640bd42b
dd0sxx <contacttheotelonis@gmail.com>
2022-05-24 14:53:40 -0600

    added autograph abi info

--- dapp/autograph-dapp/packages/contracts/src/abis.js
+++ dapp/autograph-dapp/packages/contracts/src/abis.js
@@ -1,9 +1,13 @@
-import erc20Abi from "./abis/erc20.json";
-import ownableAbi from "./abis/ownable.json";
+import AddressBookAbi from "./abis/AddressBook.json";
+import AddressBookDepolyerAbi from "./abis/AddressBookDeployer.json";
+import AddressBookFactoryAbi from "./abis/AddressBookFactory.json";
+import SubscriptionsAbi from "./abis/Subscriptions.json";
 
 const abis = {
-  erc20: erc20Abi,
-  ownable: ownableAbi,
+  AddressBook: AddressBookAbi,
+  AddressBookDepolyer: AddressBookDepolyerAbi,
+  AddressBookFactory: AddressBookFactoryAbi,
+  Subscriptions: SubscriptionsAbi,
 };
 
 export default abis;
