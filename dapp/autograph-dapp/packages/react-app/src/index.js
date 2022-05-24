28756faf5838a8754c493960626f5ccf3a3c5705
dd0sxx <contacttheotelonis@gmail.com>
2022-05-24 15:45:29 -0600

    commented out the graph stuff for now

--- dapp/autograph-dapp/packages/react-app/src/index.js
+++ dapp/autograph-dapp/packages/react-app/src/index.js
@@ -18,17 +18,17 @@ const config = {
 
 // You should replace this url with your own and put it into a .env file
 // See all subgraphs: https://thegraph.com/explorer/
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
-});
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
+// });
 
 ReactDOM.render(
   <React.StrictMode>
     <DAppProvider config={config}>
-      <ApolloProvider client={client}>
+      {/* <ApolloProvider client={client}> */}
         <App />
-      </ApolloProvider>
+      {/* </ApolloProvider> */}
     </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root"),
